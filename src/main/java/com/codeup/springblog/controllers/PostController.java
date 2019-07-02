c889f4e306e80711654eff0da246b2e248488dc5
Justin Reich <justinreich.dev@gmail.com>
2019-07-02 13:58:27 -0500

    form-model-exercise solution

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -32,22 +32,21 @@ public class PostController {
     }
 
     @GetMapping("/posts/create")
-    public String create() {
+    public String create(Model model) {
+        model.addAttribute("post", new Post());
         return "posts/create";
     }
 
     @PostMapping("/posts/create")
-    public String insert(@RequestParam String title, @RequestParam String body) {
+    public String insert(@ModelAttribute Post post) {
         User author = userDao.findOne(1L);
-        Post newPost = new Post(title, body, author);
-        postDao.save(newPost);
+        post.setAuthor(author);
+        postDao.save(post);
         return "redirect:/posts";
     }
 
     @GetMapping("/posts/{id}/edit")
     public String edit(@PathVariable long id, Model model) {
-        Post post = postDao.findOne(id);
-        System.out.println(post);
         model.addAttribute("post", postDao.findOne(id));
         return "posts/edit";
     }
@@ -55,11 +54,8 @@ public class PostController {
     @PostMapping("/posts/{id}/edit")
     public String update(
             @PathVariable long id,
-            @RequestParam String title,
-            @RequestParam String body) {
-
-        Post postToEdit = new Post(id, title, body);
-        postDao.save(postToEdit);
+            @ModelAttribute Post post) {
+        postDao.save(post);
         return "redirect:/posts";
     }
 
